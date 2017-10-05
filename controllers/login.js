d859179d07e9da0380c7abb8d89dc413505c4b9e
jeneser <jeneserwang@gmail.com>
2017-10-05 11:06:53 +0800

    Fix object parse, fix mongo findOne, ok!

--- controllers/login.js
+++ controllers/login.js
@@ -10,8 +10,9 @@ var User = require('../models/user');
  * @param {*} code 用户登录凭证
  * @param {*} encryptedData 包括敏感数据在内的完整用户信息的加密数据
  * @param {*} iv 加密算法的初始向量
+ * @return {*} bind token
  */
-exports.login = function (req, res, next) {
+exports.login = function(req, res, next) {
   var code = req.body.code;
   var encryptedData = req.body.encryptedData;
   var iv = req.body.iv;
@@ -40,25 +41,37 @@ exports.login = function (req, res, next) {
       var pc = new WXBizDataCrypt(config.appId, loginInfo.session_key);
       var userInfo = pc.decryptData(encryptedData, iv);
 
-      return Promise.resolve(JSON.parse(userInfo));
+      return userInfo;
     })
     .then(userInfo => {
       // 验证用户是否存在
-      User.findOne({
-        openid: userInfo.openid
-      }, (err, person) => {
+      User.findOne(
+        {
+          openid: userInfo.openid
+        },
+        (err, person) => {
+          if (err) {
+            res.status(500).json({
+              code: 500,
+              errMsg: '登录出错，请联系开发人员'
+            });
+          }
 
-        // 生成会话token，有效期7天
-        var token = jwt.sign({
-          openid: userInfo.openid,
-          nickName: userInfo.nickName
-        }, config.jwtSecret, {
-          expiresIn: '7d'
-        });
+          // 生成会话token，有效期7天
+          var token = jwt.sign(
+            {
+              openid: userInfo.openid,
+              nickName: userInfo.nickName
+            },
+            config.jwtSecret,
+            {
+              expiresIn: '7d'
+            }
+          );
 
-        if (err) {
-          // 不存在，注册并返回绑定信息和token
-          new User({
+          if (!person) {
+            // 不存在，注册并返回绑定信息和token
+            new User({
               openid: userInfo.openid,
               nickName: userInfo.nickName,
               gender: userInfo.gender,
@@ -66,29 +79,30 @@ exports.login = function (req, res, next) {
               avatarUrl: userInfo.avatarUrl,
               bind: false
             })
-            .save()
-            .then(() => {
-              res.status(201).json({
-                code: 201,
-                msg: '登录成功',
-                data: {
-                  bind: false,
-                  token: token
-                }
+              .save()
+              .then(() => {
+                res.status(201).json({
+                  code: 201,
+                  msg: '登录成功',
+                  data: {
+                    bind: false,
+                    token: token
+                  }
+                });
               });
+          } else {
+            // 已存在，返回绑定信息和token
+            res.status(200).json({
+              code: 200,
+              msg: '登录成功',
+              data: {
+                bind: person.bind,
+                token: token
+              }
             });
-        } else {
-          // 已存在，返回绑定信息和token
-          res.status(200).json({
-            code: 200,
-            msg: '登录成功',
-            data: {
-              bind: person.bind,
-              token: token
-            }
-          });
+          }
         }
-      });
+      );
     })
     .catch(() => {
       res.status(500).json({
