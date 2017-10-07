26b90fd6149250ca495dee5cd5f0b3124f1c9482
jeneser <jeneserwang@gmail.com>
2017-10-07 17:22:57 +0800

    Fix user find

--- controllers/user.js
+++ controllers/user.js
@@ -20,15 +20,28 @@ exports.user = function (req, res, next) {
     openId: openId
   })
     .then(person => {
-      res.status(200).json({
-        statusCode: 200,
-        msg: '查询成功',
-        data: person
-      });
+      if(person) {
+        var userInfo = {
+          nickName: person.nickName,
+          studentId: person.studentId,
+          name: person.name,
+          bind: person.bind
+        }
+        res.status(200).json({
+          statusCode: 200,
+          msg: '查询成功',
+          data: userInfo
+        });
+      } else {
+        res.status(404).json({
+          statusCode: 404,
+          errMsg: '用户不存在'
+        });
+      }
     })
     .catch(err => {
-      res.status(404).json({
-        statusCode: 404,
+      res.status(500).json({
+        statusCode: 500,
         errMsg: '用户不存在'
       });
     })
