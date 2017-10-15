6a1bcbfda325d97a847b52ba2686ed45b1e35565
jeneser <jeneserwang@gmail.com>
2017-10-15 14:58:24 +0800

    Fix HPUUrplogin

--- vendor/HPUUrpLogin.js
+++ vendor/HPUUrpLogin.js
@@ -25,8 +25,7 @@ require('superagent-charset')(request);
 // 配置
 var config = {
   // RSA加密参数
-  KEY:
-    'D41F1B452440585C5D1F853C7CBCB2908CFF324B43A42D7D77D2BB28BD64E2D098079B477D23990E935386FF73CCF865E0D84CE64793306C4083EADECFE36BCC89873EC2BA37D6CA943CB03BA5B4369EE7E31C3539DEA67FF8BF4A5CEE64EB3FD0639E78044B12C7B1D07E86EB7BCF033F78947E0ADE5653B9A88B33AFEB53BD',
+  KEY: 'D41F1B452440585C5D1F853C7CBCB2908CFF324B43A42D7D77D2BB28BD64E2D098079B477D23990E935386FF73CCF865E0D84CE64793306C4083EADECFE36BCC89873EC2BA37D6CA943CB03BA5B4369EE7E31C3539DEA67FF8BF4A5CEE64EB3FD0639E78044B12C7B1D07E86EB7BCF033F78947E0ADE5653B9A88B33AFEB53BD',
   EXP: 65537,
 
   // OCR参数
@@ -56,35 +55,28 @@ var config = {
   },
 
   // VPN参数
-  vpnLoginUrl:
-    'https://vpn.hpu.edu.cn/por/login_psw.csp?sfrnd=2346912324982305&encrypt=1',
+  vpnLoginUrl: 'https://vpn.hpu.edu.cn/por/login_psw.csp?sfrnd=2346912324982305&encrypt=1',
   vpnLoginHeader: {
     Host: 'vpn.hpu.edu.cn',
     Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'User-Agent':
-      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
     Connection: 'keep-alive',
     'Upgrade-Insecure-Requests': '1',
     'Cache-Control': 'max-age=0',
-    Referer:
-      'https://vpn.hpu.edu.cn/por/login_psw.csp?rnd=0.4288785251262913#http%3A%2F%2Fvpn.hpu.edu.cn%2F',
-    Cookie:
-      'language=en_US; TWFID=1683ff4c80034a2e; collection=%7Bauto_login_count%3A0%7D; VpnLine=http%3A%2F%2Fvpn.hpu.edu.cn%2F; g_LoginPage=login_psw; VisitTimes=0; haveLogin=0'
+    Referer: 'https://vpn.hpu.edu.cn/por/login_psw.csp?rnd=0.4288785251262913#http%3A%2F%2Fvpn.hpu.edu.cn%2F',
+    Cookie: 'language=en_US; TWFID=1683ff4c80034a2e; collection=%7Bauto_login_count%3A0%7D; VpnLine=http%3A%2F%2Fvpn.hpu.edu.cn%2F; g_LoginPage=login_psw; VisitTimes=0; haveLogin=0'
   },
 
   // 教务处URP系统参数
   // urpIndex: 'https://vpn.hpu.edu.cn/web/1/http/0/218.196.240.97/',
-  urpLoginUrl:
-    'https://vpn.hpu.edu.cn/web/1/http/1/218.196.240.97/loginAction.do',
-  urpVerCode:
-    'https://vpn.hpu.edu.cn/web/0/http/1/218.196.240.97/validateCodeAction.do?random=0.5239535101287284',
+  urpLoginUrl: 'https://vpn.hpu.edu.cn/web/1/http/1/218.196.240.97/loginAction.do',
+  urpVerCode: 'https://vpn.hpu.edu.cn/web/0/http/1/218.196.240.97/validateCodeAction.do?random=0.5239535101287284',
   urpLoginHeader: {
     Host: 'vpn.hpu.edu.cn',
-    'User-Agent':
-      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0',
     Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
@@ -188,8 +180,9 @@ function ocr(agent, fileName) {
  * @param {Number} jwcPassWord 教务处密码
  * @param {String} url 要访问的教务资源
  * @param {String} method 请求方法
+ * @return {Promise} Promise
  */
-exports.login = function(params) {
+exports.login = function (params) {
   var params = params || {};
 
   // 禁用https
@@ -205,50 +198,50 @@ exports.login = function(params) {
     return (
       // 登录VPN
       agent
-        .post(config.vpnLoginUrl)
-        .set(config.vpnLoginHeader)
-        .type('form')
-        .send({
-          svpn_name: params.studentId
-        })
-        .send({
-          svpn_password: rsa.encrypt(params.vpnPassWord)
-        })
-        .redirects()
-        // 识别URP验证码
-        .then(() => {
-          return Promise.resolve(ocr(agent, params.studentId));
-        })
-        // 登录URP
-        .then(verCodeIdentified => {
-          return agent
-            .post(config.urpLoginUrl)
-            .set(config.urpLoginHeader)
-            .type('form')
-            .send({
-              zjh1: '',
-              tips: '',
-              lx: '',
-              evalue: '',
-              eflag: '',
-              fs: '',
-              dzslh: ''
-            })
-            .send({
-              zjh: params.studentId,
-              mm: params.jwcPassWord,
-              v_yzm: params.verCodeIdentified
-            })
-            .redirects();
-        })
-        // 登录成功,访问教务资源
-        .then(() => {
-          if (params.method.toLowerCase() === 'post') {
-            return agent.post(url).charset('gbk');
-          } else {
-            return agent.get(url).charset('gbk');
-          }
-        })
+      .post(config.vpnLoginUrl)
+      .set(config.vpnLoginHeader)
+      .type('form')
+      .send({
+        svpn_name: params.studentId
+      })
+      .send({
+        svpn_password: rsa.encrypt(params.vpnPassWord)
+      })
+      .redirects()
+      // 识别URP验证码
+      .then(() => {
+        return Promise.resolve(ocr(agent, params.studentId));
+      })
+      // 登录URP
+      .then(verCodeIdentified => {
+        return agent
+          .post(config.urpLoginUrl)
+          .set(config.urpLoginHeader)
+          .type('form')
+          .send({
+            zjh1: '',
+            tips: '',
+            lx: '',
+            evalue: '',
+            eflag: '',
+            fs: '',
+            dzslh: ''
+          })
+          .send({
+            zjh: params.studentId,
+            mm: params.jwcPassWord,
+            v_yzm: verCodeIdentified
+          })
+          .redirects();
+      })
+      // 登录成功,访问教务资源 TODO:判断是否访问成功
+      .then(() => {
+        if (params.method.toLowerCase() === 'post') {
+          return Promise.resolve(agent);
+        } else {
+          return agent.get(params.url).charset('gbk');
+        }
+      })
     );
   } else {
     return Promise.reject('参数错误');
