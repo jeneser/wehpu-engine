33073afd27a4e59a63446c5559092c350cb07770
jeneser <jeneserwang@gmail.com>
2017-10-16 19:49:54 +0800

    Add classroom query, Such a difficult process

--- controllers/classroom.js
+++ controllers/classroom.js
@@ -2,6 +2,7 @@ var crypto = require('crypto');
 var config = require('../config');
 var cheerio = require('cheerio');
 var HPUUrpLogin = require('../vendor/HPUUrpLogin');
+var handleClassroom = require('../common/classroom');
 
 var User = require('../models/user');
 
@@ -14,7 +15,7 @@ var User = require('../models/user');
  * @param {String} [openId] 包含在token中的openId
  */
 exports.classroom = function (req, res, next) {
-  var openId = req.jwtPayload.openId;
+  // var openId = req.jwtPayload.openId;
   var building = req.body.building;
   var weekly = req.body.weekly;
   var section = req.body.section;
@@ -22,7 +23,7 @@ exports.classroom = function (req, res, next) {
 
   // 检验参数
   var _section = section.split(',');
-  var validator = (!!building && !!weekly && !!section && !!week) && (parseInt(building) > 0 && parseInt(building) < 19) && (parseInt(weekly) > 0 && parseInt(weekly) < 30) && (parseInt(week) > 0 && parseInt(week) < 8) && section.search(',') !== -1;
+  var validator = (!!building && !!weekly && !!section && !!week) && (parseInt(building) > 0 && parseInt(building) < 19) && (parseInt(weekly) > 0 && parseInt(weekly) < 30) && (parseInt(week) > 0 && parseInt(week) < 8) && (parseInt(section) > 0 && parseInt(section) < 11);
 
   if (!validator) {
     res.status(400).json({
@@ -38,35 +39,19 @@ exports.classroom = function (req, res, next) {
   // 查询参数
   var classroomParams = {
     // 用户输入
-    building: building,
-    weekly: weekly,
-    section: section,
-    week: week,
+    zxJxl: building,
+    zxZc: weekly,
+    zxJc: section,
+    zxxq: week,
     // 学年
     zxxnxq: zxxnxq,
     // 南校区
-    zxXaq: 01,
+    zxXaq: '01',
     // 页码参数，结果全部显示在第一页
-    pageSize: 200,
-    page: 1,
-    currentPage: 1,
+    pageSize: '20',
+    page: '1',
+    currentPage: '1',
     pageNo: ''
-  }
-
-  // 请求资源配置
-  var agentConfig = {
-    url: 'https://vpn.hpu.edu.cn/web/1/http/2/218.196.240.97/xszxcxAction.do?oper=tjcx',
-    header: {
-      Host: 'vpn.hpu.edu.cn',
-      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0',
-      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Accept-Encoding': 'gzip, deflate, br',
-      Referer: 'https://vpn.hpu.edu.cn/web/1/http/2/218.196.240.97/xszxcxAction.do?oper=ld&xqh=01&jxlh=',
-      Connection: 'keep-alive',
-      'Upgrade-Insecure-Requests': '1',
-      'Cache-Control': 'max-age=0'
-    }
   }
 
   // 访问资源
@@ -109,35 +94,19 @@ exports.classroom = function (req, res, next) {
     jwcPassWord: '311509040120'
   }
 
+  console.log(classroomParams);
+
   Promise.resolve(HPUUrpLogin.login({
       studentId: userInfo.studentId,
       vpnPassWord: userInfo.vpnPassWord,
       jwcPassWord: userInfo.jwcPassWord,
-      url: agentConfig.url,
       method: 'post'
     }))
-    .set(agentConfig.header)
-    .type('form')
-    .send(classroomParams)
-    .redirects()
-
-    // 测试是否访问成功
-    .then(urpContent => {
-      console.log(urpContent.text);
-      // return new Promise((resolve, reject) => {
-      //   if (/选课结果/.test(urpContent.text)) {
-      //     resolve(urpContent.text);
-      //   } else {
-      //     reject('访问失败');
-      //   }
-      // });
+    .then(agent => {
+      return Promise.resolve(handleClassroom.classroom(agent));
     })
     .catch(err => {
       console.log(err);
     })
-  // 处理课表
-  // .then(data => {
-  //   return Promise.resolve(handleCourse.course(data));
-  // })
 
 }
