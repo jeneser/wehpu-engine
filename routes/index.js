b1356c5ebd1c5e540463f71d51d7b549360ae8bc
jeneser <jeneserwang@gmail.com>
2017-10-21 15:49:45 +0800

    Fix index and docker-compose

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,13 @@
 var express = require('express');
+var os = require('os');
 var router = express.Router();
 
-var mongoose = require('mongoose');
-var Index = require('../models/index');
-var os = require('os');
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express!', os: os.hostname() });
-});
-
-// test c
-router.get('/c', function(req, res, next) {
-  var q = req.query.q;
-  var indexModel = new Index({
-    title: q
-  })
-    .save()
-    .then(function(val) {
-      res.json({
-        msg: 'ok!',
-        os: os.hostname()
-      });
-    });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Wehpu!',
+    os: os.hostname()
+  });
 });
 
 module.exports = router;
