88b1476d3b8087cfee44af92693386735bfe9880
jeneser <jeneserwang@gmail.com>
2017-10-22 11:28:19 +0800

    Add static dir

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '.well-known/acme-challenge')));
 
 app.use('/', index);
 app.use('/api/users', users);
