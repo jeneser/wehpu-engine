264cfa2432ac3809de3068cf1c5830a3306c1566
jeneser <jeneserwang@gmail.com>
2017-10-29 12:22:09 +0800

    Handle 404

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/', function (req, res, next) {
   });
 });
 
+// 404
+router.get('*', function (req, res, next) {
+  res.status(404).json({
+    statusCode: 404,
+    errMsg: '请求错误'
+  });
+});
+
 module.exports = router;
