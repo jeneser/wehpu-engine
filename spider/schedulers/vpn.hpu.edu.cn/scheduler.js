ffa7b5a23fa07666390205456a6786e06e1f90f4
jeneser <jeneserwang@gmail.com>
2017-11-03 23:27:30 +0800

    Clean notice scheduler

--- spider/schedulers/vpn.hpu.edu.cn/scheduler.js
+++ spider/schedulers/vpn.hpu.edu.cn/scheduler.js
@@ -8,10 +8,10 @@ var main = require('./main');
  * vpn.hpu.edu.cn
  * 星期一/星期三/星期五 凌晨2:30
  */
-module.exports = function () {
-  return schedule.scheduleJob(config.rule, () => {
+exports.run = function () {
+  schedule.scheduleJob(config.rule, () => {
     var t = new Date();
-    logger.info('Started', '教务公告定时任务', t.toISOString());
+    logger.info('Started', '教务公告定时任务');
 
     // 执行主任务
     Promise
@@ -20,7 +20,7 @@ module.exports = function () {
         logger.info('Completed', '教务公告定时任务', (new Date() - t) + 'ms');
       })
       .catch(err => {
-        logger.error('Completed', '教务公告定时任务失败:' + err);
+        logger.error('Completed', '教务公告定时任务失败', err, (new Date() - t) + 'ms');
       });
   });
 }
