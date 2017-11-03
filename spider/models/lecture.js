6cb4ee7ec163d681d3c6e1ba13ddbf974614aac1
jeneser <jeneserwang@gmail.com>
2017-11-03 23:29:52 +0800

    Add lecture models

---
+++ spider/models/lecture.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var lectureSchema = new Schema({
+  // 标题
+  title: String,
+  // 主讲人
+  speaker: String,
+  // 内容
+  content: String,
+  // 位置
+  place: String,
+  // 时间
+  time: {
+    type: String,
+    default: Date.now()
+  }
+});
+
+var Lecture = mongoose.model('Lecture', lectureSchema);
+
+module.exports = Lecture;
