58eee1b0750a8c323d83a884c812d9c89fc4dd2c
jeneser <jeneserwang@gmail.com>
2017-11-11 21:31:34 +0800

    Copy config.default

--- config.default.js
+++ config.default.js
@@ -4,12 +4,14 @@
  * 并将以下参数配置完整，重要信息请妥善保管
  */
 var config = {
+  // 调试模式
+  debug: false,
+
   // port
-  port: process.env.PORT || '3000',
+  port: '3000',
 
   // mongodb
-  mongodb:
-    'mongodb://mongodb-master:27017,mongodb-slave1:27017,mongodb-slave2:27017/wehpu?replicaSet=wehpu',
+  mongodb: 'mongodb://mongodb-master:27017,mongodb-slave1:27017,mongodb-slave2:27017/wehpu?replicaSet=wehpu',
 
   // 小程序ID 需妥善保管!
   appId: '',
@@ -29,12 +31,40 @@ var config = {
   // 通用密钥 需妥善保管!
   commonSecret: '',
 
-  // 阿里云OSS 需妥善保管!
+  // 阿里云OSS
   aliOSS: {
-    region: '',
+    region: 'oss-cn-shanghai',
     accessKeyId: '',
     accessKeySecret: '',
-    bucket: ''
+    bucket: 'wehpu-engine'
+  },
+
+  // github token 需妥善保管!
+  githubToken: '',
+
+  // 最大上传尺寸 20M > 20971520 bytes
+  limitUploadSize: 20971520,
+
+  // 日志
+  log4js: {
+    // 文件名
+    filename: 'logs/engine-cheese.log',
+    // 10M
+    maxLogSize: 10485760,
+    // 回滚三个备份
+    backups: 3
+  },
+
+  // 校历
+  calendar: {
+    // 当前学期 秋季:2017-2018-1-1 春季:2017-2018-2-1
+    currentTerm: '2017-2018-1-1',
+    // 开学日期
+    termStart: '2017-9-4',
+    // 放假日期
+    termEnd: '2018-1-22',
+    // 总周次
+    totalWeekly: 20
   }
 }
 
--- spider/config.default.js
+++ spider/config.default.js
@@ -1,5 +1,17 @@
 var config = {
-  debug: false,
+  debug: true,
+
+  // mongodb
+  mongodb: 'mongodb://mongodb-master:27017,mongodb-slave1:27017,mongodb-slave2:27017/wehpu?replicaSet=wehpu',
+
+  // 通用加密算法 需妥善保管!
+  commonAlgorithm: '',
+
+  // 通用密钥 需妥善保管!
+  commonSecret: '',
+
+  // 最大上传尺寸 20M > 20971520 bytes
+  limitUploadSize: 20971520,
 
   // 日志
   log4js: {
@@ -13,10 +25,16 @@ var config = {
 
   // 阿里云OSS
   aliOSS: {
-    region: '',
+    region: 'oss-cn-shanghai',
     accessKeyId: '',
     accessKeySecret: '',
-    bucket: ''
+    bucket: 'wehpu-engine'
+  },
+
+  // 校内资源认证帐号 加密存储
+  userInfo: {
+    studentId: '',
+    vpnPassWord: ''
   }
 }
 
