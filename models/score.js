e985c459a2c581bf1fb871b2fa057eb99095ddab
jeneser <jeneserwang@gmail.com>
2017-11-19 20:56:32 +0800

    Hot fix: fix score schema

--- models/score.js
+++ models/score.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var scoreSchema = new Schema({
-  // id
-  id: String,
-  // 空闲教室数据
+  // openId
+  openId: String,
+  // 成绩数据
   scores: Array,
   // 学期
   term: String,
