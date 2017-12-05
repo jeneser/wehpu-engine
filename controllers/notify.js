3c19c84096719d0c8232da2f76d2237d016bf944
jeneser <jeneserwang@gmail.com>
2017-12-05 20:32:08 +0800

    Hot fix: 定位通知bug

--- controllers/notify.js
+++ controllers/notify.js
@@ -68,6 +68,7 @@ exports.notify = function (req, res, next) {
           errMsg: '发送成功'
         })
       } else {
+        logger.info(data)
         return res.status(400).json({
           statusCode: 400,
           errMsg: '发送失败'
